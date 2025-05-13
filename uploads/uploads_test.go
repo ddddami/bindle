4c23193978c0f1bb5a5dedcb3bbbb3862a55a719
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-05-13 09:04:15 +0100

    Add unit tests for file upload

---
+++ uploads/uploads_test.go
@@ -0,0 +1,169 @@
+package uploads
+
+import (
+	"bytes"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestSanitizeFileName(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{"normal.txt", "normal.txt"},
+		{"file with spaces.jpg", "file_with_spaces.jpg"},
+		{"../../../etc/passwd", "......etcpasswd"},
+		{"file:with:colons.png", "filewithcolons.png"},
+		{"file*with?invalid\"chars.xxx", "filewithinvalidchars.xxx"},
+	}
+
+	for _, tc := range testCases {
+		result := santizeFileName(tc.input)
+		if result != tc.expected {
+			t.Errorf("For input '%s', expected '%s' but got '%s'", tc.input, tc.expected, result)
+		}
+	}
+}
+
+func TestSaveUploadedFile(t *testing.T) {
+	testDir := "test_uploads"
+	os.RemoveAll(testDir)
+	defer os.RemoveAll(testDir)
+
+	fileContents := []byte("test file contents")
+
+	testCases := []struct {
+		name          string
+		options       FileUploadOptions
+		expectError   bool
+		validateSaved func(*SavedFile) bool
+	}{
+		{
+			name: "basic upload with default options",
+			options: FileUploadOptions{
+				DestinationDir: testDir,
+				MaxSize:        10 * 1024 * 1024,
+				AllowedExts:    []string{"jpg", "jpeg", "png", "gif", "txt"},
+			},
+			expectError: false,
+			validateSaved: func(sf *SavedFile) bool {
+				return strings.HasSuffix(sf.SavedName, ".txt") &&
+					sf.Size == int64(len(fileContents)) &&
+					sf.OriginalName == "test.txt"
+			},
+		},
+		{
+			name: "fixed filename",
+			options: FileUploadOptions{
+				DestinationDir:    testDir,
+				RandomizeFilename: false,
+				MaxSize:           10 * 1024 * 1024,
+			},
+			validateSaved: func(sf *SavedFile) bool {
+				return sf.SavedName == "test.txt" &&
+					sf.Size == int64(len(fileContents))
+			},
+		},
+		{
+			name: "randomized filename",
+			options: FileUploadOptions{
+				DestinationDir:    testDir,
+				RandomizeFilename: true,
+				MaxSize:           10 * 1024 * 1024,
+			},
+			validateSaved: func(sf *SavedFile) bool {
+				return len(sf.SavedName) > len("test.txt") &&
+					sf.Size == int64(len(fileContents)) &&
+					sf.OriginalName == "test.txt"
+			},
+		},
+		{
+			name: "file size limit too small",
+			options: FileUploadOptions{
+				DestinationDir:    testDir,
+				MaxSize:           5,
+				RandomizeFilename: true,
+			},
+			expectError: true,
+			validateSaved: func(sf *SavedFile) bool {
+				return false
+			},
+		},
+
+		{
+			name: "with extension restriction - allowed",
+			options: FileUploadOptions{
+				DestinationDir:    testDir,
+				AllowedExts:       []string{"txt", "doc"},
+				RandomizeFilename: true,
+				MaxSize:           10 * 1024 * 1024,
+			},
+			expectError: false,
+			validateSaved: func(sf *SavedFile) bool {
+				return strings.HasSuffix(sf.SavedName, ".txt")
+			},
+		},
+
+		{
+			name: "with extension restriction - disallowed",
+			options: FileUploadOptions{
+				DestinationDir:    testDir,
+				AllowedExts:       []string{"jpg", "png"},
+				RandomizeFilename: true,
+				MaxSize:           10 * 1024 * 1024,
+			},
+			expectError: true,
+			validateSaved: func(sf *SavedFile) bool {
+				return false
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			fh := createTestFileHeader("test.txt", fileContents)
+
+			savedFile, err := SaveUploadedFile(fh, &tc.options)
+			if tc.expectError {
+				if err == nil {
+					t.Errorf("Expected error but got nil")
+					return
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Did not expect error but got: %v", err)
+					return
+				}
+
+				if !tc.validateSaved(savedFile) {
+					t.Errorf("Saved file validation failed")
+				}
+
+				if _, err := os.Stat(savedFile.SavedPath); os.IsNotExist(err) {
+					t.Errorf("Expected file to exist at %s, but it does not", savedFile.SavedPath)
+				}
+
+			}
+		})
+	}
+}
+
+func createTestFileHeader(filename string, content []byte) *multipart.FileHeader {
+	buf := new(bytes.Buffer)
+	writer := multipart.NewWriter(buf)
+	part, _ := writer.CreateFormFile("file", filename)
+	part.Write(content)
+	writer.Close()
+
+	req, _ := http.NewRequest("POST", "/", buf)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	req.ParseMultipartForm(32 << 20)
+	file := req.MultipartForm.File["file"][0]
+
+	return file
+}
